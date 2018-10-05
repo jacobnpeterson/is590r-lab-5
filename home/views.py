2d939705f1cf353129cec47cf739266d3f324272
Ubuntu <ubuntu@ip-172-31-32-59.ec2.internal>
2018-10-05 20:58:59 +0000

    trying a change

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! Nicely done. Another change.")
+    return HttpResponse("Hello, world! If you are reading this, the internet works!")
 
